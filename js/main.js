419835e816292098032ab6eda27c44ced12d74db
jbpin <jibet.pin@gmail.com>
2015-09-22 00:11:39 +0200

    added map

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$(document).foundation();
 
+$(document).one('open.fndtn.reveal', '[data-reveal]', function () {
+  var modal = $(this);
+  var ifr = modal.find('iframe');
+  ifr[0].src = ifr[0].src;
+});
